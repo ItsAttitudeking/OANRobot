3a31b07d5b6ccf50e2107c1926c0aab69af34e8f
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-12-15 17:11:26 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop YoneRobot
-net start YoneRobot
+net stop OANRobot
+net start OANRobot
