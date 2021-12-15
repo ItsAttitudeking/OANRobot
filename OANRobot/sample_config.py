a77af00674c72aae6b8c4382d213ab787ca24638
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-12-15 16:47:44 +0530

    Update sample_config.py

--- OANRobot/sample_config.py
+++ OANRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/YoneRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/OANRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    OWNER_ID = 1836386247  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Alone_Shaurya_king"
+    SUPPORT_CHAT = "OAN_Support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001461752870
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001461752870
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
