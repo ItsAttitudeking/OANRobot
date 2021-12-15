a447cd0c3070427555a8d87e251edc7f48d08a5f
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-12-15 16:50:52 +0530

    Update events.py

--- OANRobot/events.py
+++ OANRobot/events.py
@@ -8,8 +8,8 @@ from pathlib import Path
 from telethon import events
 
 from pymongo import MongoClient
-from YoneRobot import MONGO_DB_URI
-from YoneRobot import telethn
+from OANRobot import MONGO_DB_URI
+from OANRobot import telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
@@ -146,7 +146,7 @@ def bot(**args):
     return decorator
 
 
-def YoneRobot(**args):
+def OANRobot(**args):
     pattern = args.get("pattern", None)
     disable_edited = args.get("disable_edited", False)
     ignore_unsafe = args.get("ignore_unsafe", False)
@@ -182,32 +182,32 @@ def load_module(shortname):
         pass
     elif shortname.endswith("_"):
         import importlib
-        import YoneRobot.events
+        import OANRobot.events
 
-        path = Path(f"YoneRobot/modules/{shortname}.py")
-        name = "YoneRobot.modules.{}".format(shortname)
+        path = Path(f"OANRobot/modules/{shortname}.py")
+        name = "OANRobot.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
         print("Successfully imported " + shortname)
     else:
         import importlib
-        import YoneRobot.events
+        import OANRobot.events
 
-        path = Path(f"YoneRobot/modules/{shortname}.py")
-        name = "YoneRobot.modules.{}".format(shortname)
+        path = Path(f"OANRobot/modules/{shortname}.py")
+        name = "OANRobot.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.register = register
-        mod.YoneRobot = YoneRobot
+        mod.OANRobot = OANRobot
         mod.tbot = telethn
         mod.logger = logging.getLogger(shortname)
         spec.loader.exec_module(mod)
-        sys.modules["YoneRobot.modules." + shortname] = mod
+        sys.modules["OANRobot.modules." + shortname] = mod
         print("Successfully imported " + shortname)
 
 
-path = "YoneRobot/modules/*.py"
+path = "OANRobot/modules/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
