15a8585819d18615118575484a2785fc2b143ee7
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-12-15 15:52:14 +0530

    Update music.py

--- OANRobot/modules/music.py
+++ OANRobot/modules/music.py
@@ -61,7 +61,7 @@ async def ytmusic(client, message: Message):
         return
     c_time = time.time()
     file_stark = f"{ytdl_data['id']}.mp4"
-    capy = f"**Video Name ➠** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}` \n**Link :** `{mo}`"
+    capy = f"**Video Name ➠** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}` \n**Link :** `{mo}` \n\n Powered By : @Attitude_Network"
     await client.send_video(
         message.chat.id,
         video=open(file_stark, "rb"),
