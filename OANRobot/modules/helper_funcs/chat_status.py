5012fcce727064115fa887cff2865086107f9238
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-12-15 17:55:29 +0530

    Update chat_status.py

--- OANRobot/modules/helper_funcs/chat_status.py
+++ OANRobot/modules/helper_funcs/chat_status.py
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from YoneRobot.modules import connection
+from OANRobot.modules import connection
 
 connected = connection.connected
