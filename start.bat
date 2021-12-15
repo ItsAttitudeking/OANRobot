789be89a059ba1bba2fbb828c661efc99d1cd7a9
Attitude king <93428427+ItsAttitudeking@users.noreply.github.com>
2021-12-15 17:10:47 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Yone Robot
+TITLE OAN Robot
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m YoneRobot
+env\scripts\activate.bat && py -m OANRobot
